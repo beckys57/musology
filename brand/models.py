ae5d7a08d574db00b1b10d5a6c1b40dc0296f404
Becky Smith <beckysmith@piratestudios.co.uk>
2020-05-03 18:04:41 +0100

    Bugfix brand colour

--- brand/models.py
+++ brand/models.py
@@ -12,7 +12,7 @@ class Brand(models.Model):
 
   @property
   def display_attrs(self):
-    return {"name": self.name, "colour": self.get_colour_display()}
+    return {"name": self.name, "colour": self.colour}
 
 class BrandedModel(models.Model):
   brand = models.ForeignKey(to='Brand', null=True, blank=True, on_delete=models.SET_NULL)
