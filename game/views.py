ea14062a43c00a3d610af05cadedeb7a137594b5
Becky Smith <beckysmith@piratestudios.co.uk>
2020-05-03 18:32:50 +0100

    Initialize game if not exists

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Game
 from brand.models import Band, Brand
 from genres.models import Genre
 from locations.models import City, Location
@@ -7,6 +8,11 @@ def take_turn(self):
   print("Taking turn...")
 
 def index(request):
+  game = Game.objects.first()
+  if not game:
+    game = Game.objects.create()
+    game.initialize()
+    
   map_data = [City.build_display_attrs()]
   genre_data = {g.id: g.display_attrs for g in Genre.objects.all()}
   brand_data = {b.id: b.display_attrs for b in Brand.objects.all()}
