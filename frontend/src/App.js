6a01485e883df238e2a2efa59874a442bb8e6903
Martin Alcock <martin.alcock87@gmail.com>
2020-05-14 18:37:09 +0100

    Some updates from yesterday

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,16 @@ import axios from "axios"
 
 
 
-export function Sidebar() {
+export function SidebarContent(venue) {
   return (
-    <>
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary" onClick={takeTurn}>Go somewhere</a>
-    </>
+    <div className="card">
+      <img className="card-img-top" src="..." alt="Card image cap"/>
+      <div className="card-body">
+        <h5 className="card-title">Venue: {venue.name}</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="s#" className="btn btn-primary" onClick={takeTurn}>Go somewhere</a>
+      </div>
+    </div>
   )
 }
 
@@ -36,7 +39,7 @@ export function VenuePopup ({selectedVenue, setSelectedVenue}) {
 
 
 
-export function Map() {
+export function Map({selectedVenue, setSelectedVenue, clickOnVenue}) {
   const [viewport, setViewport] = useState({
     latitude: 0,
     longitude: 0,
@@ -44,7 +47,6 @@ export function Map() {
     height: "100vh",
     zoom: 0,
   });
-  const [selectedVenue, setSelectedVenue] = useState(null);
   const [markers, setMarkers] = useState(null);
   const [apiData, setApiData] = useState({})
 
@@ -70,7 +72,8 @@ export function Map() {
               className="marker-btn"
               onClick={e => {
                 e.preventDefault();
-                setSelectedVenue(venue);
+                clickOnVenue(venue);
+
               }}
             >
               <img src="/skateboarding.svg" alt="Skate Venue Icon" />
@@ -94,80 +97,6 @@ export function Map() {
       window.removeEventListener("keydown", listener);
     };
   }, []);
-
-  // NB: This is just some example data in the correct format, please replace :)
-  const [postData, setPostData] = useState({"locations": [
-            {
-            "id": 1,
-            "events": [
-            {
-            "slot": 1,
-            "kind": "gig",
-            "band_ids": [1],
-            "promoter_ids": [],
-            "people_ids": [],
-            },
-            {
-            "slot": 2,
-            "kind": "",
-            "band_ids": [],
-            "promoter_ids": [],
-            "people_ids": [],
-            },
-            {
-            "slot": 3,
-            "kind": "gig",
-            "band_ids": [2],
-            "promoter_ids": [],
-            "people_ids": [],
-            },
-            {
-            "slot": 4,
-            "kind": "deep clean upgrade",
-            "band_ids": [],
-            "promoter_ids": [],
-            "people_ids": [5],
-            },
-            ],
-            "updates": {
-            "entry_price": 12,
-            "name": "Badger"
-            }
-            },
-            {
-            "id": 2,
-            "events": [
-            {
-            "slot": 1,
-            "kind": "training",
-            "band_ids": [1],
-            "promoter_ids": [],
-            "people_ids": [15, 16, 17, 18],
-            },
-            {
-            "slot": 2,
-            "kind": "training",
-            "band_ids": [],
-            "promoter_ids": [],
-            "people_ids": [15, 16, 17, 18],
-            },
-            {
-            "slot": 3,
-            "kind": "training",
-            "band_ids": [2],
-            "promoter_ids": [],
-            "people_ids": [15, 16, 17, 18],
-            },
-            {
-            "slot": 4,
-            "kind": "deep clean upgrade",
-            "band_ids": [],
-            "promoter_ids": [],
-            "people_ids": [15, 16, 17, 18],
-            },
-            ],
-            }
-            ]})
 
   // NB: Commented the map out, as it was complaining about me not having API access to your Mapbox account.
   // Can just put this straight back in and remove the Take turn button above
@@ -197,16 +126,97 @@ export function Map() {
 }
 
 export default function App() {
+  const [selectedVenue, setSelectedVenue] = useState(null);
+
+  // NB: This is just some example data in the correct format, please replace :)
+  const [postData, setPostData] = useState({"locations": [
+    {
+    "id": 1,
+    "events": [
+    {
+    "slot": 1,
+    "kind": "gig",
+    "band_ids": [1],
+    "promoter_ids": [],
+    "people_ids": [],
+    },
+    {
+    "slot": 2,
+    "kind": "",
+    "band_ids": [],
+    "promoter_ids": [],
+    "people_ids": [],
+    },
+    {
+    "slot": 3,
+    "kind": "gig",
+    "band_ids": [2],
+    "promoter_ids": [],
+    "people_ids": [],
+    },
+    {
+    "slot": 4,
+    "kind": "deep clean upgrade",
+    "band_ids": [],
+    "promoter_ids": [],
+    "people_ids": [5],
+    },
+    ],
+    "updates": {
+    "entry_price": 12,
+    "name": "Badger"
+    }
+    },
+    {
+    "id": 2,
+    "events": [
+    {
+    "slot": 1,
+    "kind": "training",
+    "band_ids": [1],
+    "promoter_ids": [],
+    "people_ids": [15, 16, 17, 18],
+    },
+    {
+    "slot": 2,
+    "kind": "training",
+    "band_ids": [],
+    "promoter_ids": [],
+    "people_ids": [15, 16, 17, 18],
+    },
+    {
+    "slot": 3,
+    "kind": "training",
+    "band_ids": [2],
+    "promoter_ids": [],
+    "people_ids": [15, 16, 17, 18],
+    },
+    {
+    "slot": 4,
+    "kind": "deep clean upgrade",
+    "band_ids": [],
+    "promoter_ids": [],
+    "people_ids": [15, 16, 17, 18],
+    },
+    ],
+    }
+    ]})
+
+  const [sidebarContent, setSidebarContent] = useState();
+
+  function clickOnVenue(venue) {
+    setSelectedVenue(venue)
+    setSidebarContent(<SidebarContent venue={venue}></SidebarContent>)
+  }
   return (
     <>
-    <div id="map" className="col-lg-9"><Map></Map></div>
+    <div id="map" className="col-lg-9">
+      <Map selectedVenue={selectedVenue} setSelectedVenue={setSelectedVenue} clickOnVenue={clickOnVenue}></Map>
+      </div>
     <div className="col-lg-3">
 
-    <div className="card">
-      <img className="card-img-top" src="..." alt="Card image cap"/>
-      <div className="card-body"><Sidebar></Sidebar>
-      </div>
-    </div>
+
+    {sidebarContent}
     
   </div>
   </>
@@ -218,7 +228,7 @@ export default function App() {
 
 
 async function takeTurn() {
-    // console.log('Taking turn...', postData)
-    // let resp = await axios.post('http://localhost:8000/take_turn/', postData)
-    // console.log('Turn taken', resp)
-  }
+  // console.log('Taking turn...', postData)
+  // let resp = await axios.post('http://localhost:8000/take_turn/', postData)
+  // console.log('Turn taken', resp)
+}
